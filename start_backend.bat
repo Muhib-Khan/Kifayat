603c100edc6598643b019db3ac0cf8785bb1767e
Muhammad Muhib Khan <muhibkhan2410@gmail.com>
2026-08-07 02:24:37 +0500

    chore: remove stale .bat launcher scripts with outdated machine paths

--- start_backend.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\ashar\Desktop\complete-main - Copy\kifayat-backend"
-set PORT=3001
-npm start
--- start_frontend.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "C:\Users\ashar\Desktop\complete-main - Copy\kifayat-frontend"
-npm run dev
